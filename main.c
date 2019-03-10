ec34b23dd4cafa3b13f61de7b295daa06069437f
truckchen <1587986188@qq.com>
2019-03-10 11:23:46 -0500

    杭电oj
    2099

--- main.c
+++ main.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
 int main()
 {
-   char string[100];
-   int n;
-   while(scanf("%d",&n)!=EOF)
-   {
-       getchar();
-      for(int t=1;t<=n;t++)
-        {
-           gets(string);
-           int i,j;
-           int s1,s2,s3,s4,s5;
-           s1=s2=s3=s4=s5=0;
-           j=strlen(string);
-           for(i=0;i<j;i++)
-           {
-                if(string[i]=='a')
-                    s1++;
-                if(string[i]=='e')
-                    s2++;
-                if(string[i]=='i')
-                    s3++;
-                if(string[i]=='o')
-                    s4++;
-                if(string[i]=='u')
-                    s5++;
-           }
-           printf("a:%d\n",s1);
-           printf("e:%d\n",s2);
-           printf("i:%d\n",s3);
-           printf("o:%d\n",s4);
-           printf("u:%d\n",s5);
-           if(t!=n)
-            printf("\n");
-        }
-   }
+    int a,b,i;
+    int sum;
+    while(scanf("%d%d",&a,&b)!=EOF)//£¨0<a<10000, 10<b<100£©
+    {
 
-    return 0;
+
+            sum=0;
+            int sign=1;
+            if(a==0&&b==0)
+                      break;
+            for(i=0;i<100;i++)
+
+                   {
+                         sum=a*100+i;
+                         if(sum%b==0)
+                             {
+				if(sign==0)
+					printf(" ");
+				if(i<10)
+				{
+					printf("0");
+					printf("%d",i);
+				}
+				else
+					printf("%d",i);
+				sign = 0;
+			}
+		}
+		printf("\n");
+    }
+
 }
