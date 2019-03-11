44a3a65878b409faf33c5355ba0e51df59d325a4
truckchen <1587986188@qq.com>
2019-03-11 11:09:26 -0500

    杭电2046
    2019/3/12

--- main.c
+++ main.c
@@ -3,23 +3,18 @@
 
 int main()
 {
-     int m,n,i,j,t,k;
+     int i,k;
 
     while(scanf("%d",&k)!=EOF)
     {
         getchar();
-        __int64 str[50]={0,1,1};
-
-        for(j=1;j<=k;j++)
-        {
-          scanf("%d %d",&m,&n);
-          t=n-m+1;
-          for(i=3;i<=t;i++)
+        __int64 str[51]={0,1,2};
+         for(i=3;i<=k;i++)
           {
            str[i]=str[i-1]+str[i-2];
           }
-          printf("%I64d\n",str[t]);
-        }
+          printf("%I64d\n",str[k]);
+
 
     }
     return 0;
